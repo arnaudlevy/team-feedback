1fa87d581958078a7474d8ff1ccdad65575dd8d1
Arnaud Levy <arnaud@semiodesign.com>
2014-03-30 21:19:31 +0200

    fix

--- app/controllers/indicators_controller.rb
+++ app/controllers/indicators_controller.rb
@@ -2,13 +2,11 @@ class IndicatorsController < ApplicationController
   before_action :set_indicator, only: [:show, :edit, :update, :destroy]
 
   # GET /indicators
-  # GET /indicators.json
   def index
     @indicators = Indicator.all
   end
 
   # GET /indicators/1
-  # GET /indicators/1.json
   def show
   end
 
@@ -22,52 +20,35 @@ class IndicatorsController < ApplicationController
   end
 
   # POST /indicators
-  # POST /indicators.json
   def create
     @indicator = Indicator.new(indicator_params)
-
-    respond_to do |format|
-      if @indicator.save
-        format.html { redirect_to @indicator, notice: 'Indicator was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @indicator }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @indicator.errors, status: :unprocessable_entity }
-      end
+    if @indicator.save
+      redirect_to @indicator, notice: 'Indicator was successfully created.'
+    else
+      render action: 'new'
+    end
     end
   end
 
   # PATCH/PUT /indicators/1
-  # PATCH/PUT /indicators/1.json
   def update
-    respond_to do |format|
-      if @indicator.update(indicator_params)
-        format.html { redirect_to @indicator, notice: 'Indicator was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @indicator.errors, status: :unprocessable_entity }
-      end
+    if @indicator.update(indicator_params)
+      redirect_to @indicator, notice: 'Indicator was successfully updated.'
+    else
+      render action: 'edit'
     end
   end
 
   # DELETE /indicators/1
-  # DELETE /indicators/1.json
   def destroy
     @indicator.destroy
-    respond_to do |format|
-      format.html { redirect_to indicators_url }
-      format.json { head :no_content }
-    end
+    redirect_to indicators_url
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_indicator
       @indicator = Indicator.find(params[:id])
     end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
     def indicator_params
       params.require(:indicator).permit(:label)
     end
