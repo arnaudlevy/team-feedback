2a91475c8d61e51b792d33186f5ebf248bb866a3
Arnaud Levy <arnaud@semiodesign.com>
2014-07-13 22:44:27 +0200

    font hack

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,6 @@ module TeamPulsation
     config.assets.enabled = true
     config.assets.compile = true
     config.assets.initialize_on_precompile = false
-    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-    config.assets.precompile += %w( .svg .eot .woff .ttf .otf )
     
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
