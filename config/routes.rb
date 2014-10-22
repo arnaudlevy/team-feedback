ba999ef3327ce72f0f76df5a8e1005415aa7aff7
Arnaud Levy <arnaud@semiodesign.com>
2014-10-23 00:19:07 +0200

    log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,10 @@
 TeamPulsation::Application.routes.draw do
   root 'users#me'
-  get 'login' => 'user_sessions#new', as: :login
-  post 'logout' => 'user_sessions#destroy', as: :logout
   get 'me' => 'users#me', as: :me
   get 'me/edit' => 'users#edit_me', as: :edit_me
   put 'log/:indicator_id/:value' => 'log#value', as: :log
   resources :indicators
+  devise_for :users
   resources :users
   resources :teams
-
-  devise_for :users, path: "auth", path_names: { 
-    sign_in: 'login', 
-    sign_out: 'logout', 
-    password: 'secret', 
-    confirmation: 'verification', 
-    unlock: 'unblock',
-    registration: 'register', 
-    sign_up: 'cmon_let_me_in' }
-
 end
